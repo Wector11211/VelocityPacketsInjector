01ca435863bcdaa182ea746b78b4f9ae15f8d0f8
Wector11211 <wector11211@gmail.com>
2023-08-23 19:57:31 +0300

    initial commit

---
+++ velocity/src/main/java/dev/wector11211/vpi/VelocityPacketsInjectorPlugin.java
@@ -0,0 +1,28 @@
+package dev.wector11211.vpi;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
+import com.velocitypowered.api.proxy.ProxyServer;
+import org.slf4j.Logger;
+
+import java.nio.file.Path;
+
+public class VelocityPacketsInjectorPlugin {
+
+    private final ProxyServer proxy;
+    private final Logger logger;
+    private final Path dataDirectory;
+
+    @Inject
+    public VelocityPacketsInjectorPlugin(ProxyServer proxy, Logger logger, @DataDirectory Path dataDirectory) {
+        this.proxy = proxy;
+        this.logger = logger;
+        this.dataDirectory = dataDirectory;
+    }
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+    }
+}
